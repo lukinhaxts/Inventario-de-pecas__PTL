1446a9b309520ff49dcda624fa9bccf23bcb296c
Lucas Costa Souza <lucas_costasouza@yahoo.com.br>
2021-11-22 11:58:01 -0300

    Programa completo sem execução por .exe

--- Versão Final/main.py
+++ Versão Final/main.py
@@ -5,12 +5,17 @@ from functions import *
 
 def main():
 
+    print("Programa 'main.py' em execução!")
+
     dfColeta = pd.read_csv('input/coleta.txt', delimiter = ' ').rename(columns = {'Peças': 'Peças Coletadas'})
     dfColeta = dfColeta[dfColeta["Peças Coletadas"].astype('string').str.len() == 16]
-
     dfVA = pd.read_csv('input/peçasDoSistema.CSV', delimiter = ';', usecols = ["Codigo 1000", "Local"], dtype = {"Codigo 1000": np.int64}).rename(columns = {'Codigo 1000' : 'Peças VA', 'Local' : 'Local VA'})[["Peças VA", "Local VA"]]
 
+    print("Dados Coletados dos arquivos .CSV .")
+
     dfColeta = coletaParaVA(dfColeta)
+
+    print("Dados do arquivo de 'coleta.txt' transformados.")
 
     dfVA = dfVA.rename(columns = {"Peças VA" : "Peça"})
     dfColeta = dfColeta.rename(columns = {"Peças Coletadas" : "Peça"})
@@ -20,7 +25,12 @@ def main():
 
     mergeAll = pd.concat([dfMergeVA, dfMergeColeta], ignore_index=True)
 
+    print("Dados tratados.")
+    print("Preparando resultados.")
+
     dataFrameToExcel(mergeAll, "output/")
+
+    print("Resultados computados na pasta 'output'")
 
 if __name__ == "__main__":
     main()
